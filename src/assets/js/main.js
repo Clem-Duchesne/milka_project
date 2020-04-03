e1e26e94f1b5709f2cb2f2cb800f4d51377e47db
unknown <clemenceduchesne@orange.fr>
2020-04-03 16:02:19 +0200

    [js][html] add library scrollReveal with animations

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,6 +1,8 @@
+const sr = ScrollReveal();
 var plusBtn = document.getElementsByClassName('details__button')[0];
 var nutritionnalValues = document.getElementsByClassName('nutritional_values')[0];
-console.log(nutritionnalValues);
+var bannierFront = document.getElementsByClassName('presentation__page')[0];
+var bannierBack = document.getElementsByClassName('presentation__page')[1];
 
 
 function showDetails(){
@@ -9,7 +11,13 @@ function showDetails(){
     plusBtn.children[0].classList.toggle('fi-xnluxl-plus');
     plusBtn.children[0].classList.toggle('fi-xtluxl-minus-thin');
 }
+
 plusBtn.addEventListener('click', showDetails, false);
 
 
-
+sr.reveal('.presentation__button', {scale : 0.5, reset : true});
+sr.reveal('.details__product', {origin : 'left', distance : '50px', reset : true});
+sr.reveal('.details__ingredients', {origin : 'right', distance : '50px', delay : 200, reset : true});
+sr.reveal('.cow__picture', {origin : 'left', distance : '50px', reset : true});
+sr.reveal('.cow__presentation div', {origin : 'right', distance : '50px', delay : 200, reset : true});
+sr.reveal('.recipe', {origin : 'bottom', duration : 1000, distance : '50px', reset : true});
